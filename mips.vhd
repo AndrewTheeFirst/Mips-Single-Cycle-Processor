d9011384e3296d2335493811cf07f848dc674fdf
AndrewTheeFirst <03drewp@gmail.com>
2025-11-26 23:55:10 -0500

    Implemented JAL and JR mips instructions

--- mips.vhd
+++ mips.vhd
@@ -66,14 +66,15 @@ architecture arch of mips is
     end component;
 
     component mips_control 
-        port(   opcode              : in std_logic_vector(5 downto 0);
-                funct               : in std_logic_vector(5 downto 0);
-                RegDst, ALUSrc      : out std_logic;
-                Jump, Jal, Jr       : out std_logic;
-                Beq, Bne            : out std_logic;
-                MemRead, MemWrite   : out std_logic;
-                RegWrite, MemtoReg  : out std_logic;
-                ALUControl          : out std_logic_vector(3 downto 0)
+        port(   
+            opcode              : in std_logic_vector(5 downto 0);
+            funct               : in std_logic_vector(5 downto 0);
+            RegDst, ALUSrc      : out std_logic;
+            Jump, Jal, Jr       : out std_logic;
+            Beq, Bne            : out std_logic;
+            MemRead, MemWrite   : out std_logic;
+            RegWrite, MemtoReg  : out std_logic;
+            ALUControl          : out std_logic_vector(3 downto 0)
         );
     end component;
 
@@ -87,32 +88,37 @@ architecture arch of mips is
         );
     end component;
 -- declare signals (wires)
-    signal PC_OUT_WIRE      : std_logic_vector(31 downto 0);
-    signal Instruction      : std_logic_vector(31 downto 0);
-    signal RegDst_WIRE      : std_logic;
-    signal RegDst_MUXOUT    : std_logic_vector(4 downto 0);
-    signal ALUSrc_WIRE      : std_logic;
-    signal ALUSrc_MUXOUT    : std_logic_vector(31 downto 0);
-    signal Branch_MUXSEL    : std_logic;
-    signal Branch_MUXOUT    : std_logic_vector(31 downto 0);
-    signal Jump_MUXSEL      : std_logic;
-    signal Jump_MUXOUT      : std_logic_vector(31 downto 0);
-    signal SignExtendImm    : std_logic_vector(31 downto 0);
-    signal RegWrite_WIRE    : std_logic;
-    signal read_data1_WIRE  : std_logic_vector(31 downto 0);
-    signal read_data2_WIRE  : std_logic_vector(31 downto 0);
-    signal ALU_Result_WIRE  : std_logic_vector(31 downto 0);
-    signal ALUControl_WIRE  : std_logic_vector(3 downto 0);
-    signal MemWrite_WIRE    : std_logic;
-    signal MemRead_WIRE     : std_logic;
-    signal ALUZeroBit_WIRE  : std_logic;
-    signal BEQ_WIRE         : std_logic;
-    signal BNE_WIRE         : std_logic;
-    signal JumpAddr_WIRE    : std_logic_vector(31 downto 0);
-    signal PC_PLUS_FOUR     : std_logic_vector(31 downto 0);
-    signal MemtoReg_WIRE    : std_logic;
-    signal MemtoReg_MUXOUT  : std_logic_vector(31 downto 0);
-    signal ReadMemData_WIRE : std_logic_vector(31 downto 0);
+    signal PC_OUT_WIRE          : std_logic_vector(31 downto 0);
+    signal Instruction          : std_logic_vector(31 downto 0);
+    signal RegDst_WIRE          : std_logic;
+    signal RegDst_MUXOUT        : std_logic_vector(4 downto 0);
+    signal ALUSrc_WIRE          : std_logic;
+    signal ALUSrc_MUXOUT        : std_logic_vector(31 downto 0);
+    signal Branch_MUXSEL        : std_logic;
+    signal Branch_MUXOUT        : std_logic_vector(31 downto 0);
+    signal Jump_MUXSEL          : std_logic;
+    signal Jump_MUXOUT          : std_logic_vector(31 downto 0);
+    signal SignExtendImm        : std_logic_vector(31 downto 0);
+    signal RegWrite_WIRE        : std_logic;
+    signal read_data1_WIRE      : std_logic_vector(31 downto 0);
+    signal read_data2_WIRE      : std_logic_vector(31 downto 0);
+    signal ALU_Result_WIRE      : std_logic_vector(31 downto 0);
+    signal ALUControl_WIRE      : std_logic_vector(3 downto 0);
+    signal MemWrite_WIRE        : std_logic;
+    signal MemRead_WIRE         : std_logic;
+    signal ALUZeroBit_WIRE      : std_logic;
+    signal BEQ_WIRE             : std_logic;
+    signal BNE_WIRE             : std_logic;
+    signal JumpAddr_WIRE        : std_logic_vector(31 downto 0);
+    signal PC_PLUS_FOUR         : std_logic_vector(31 downto 0);
+    signal MemtoReg_WIRE        : std_logic;
+    signal MemtoReg_MUXOUT      : std_logic_vector(31 downto 0);
+    signal ReadMemData_WIRE     : std_logic_vector(31 downto 0);
+    signal Jal_WIRE             : std_logic;
+    signal Jal_MemtoReg_MUXOUT  : std_logic_vector(31 downto 0);
+    signal Jal_RegDst_MUXOUT    : std_logic_vector(4 downto 0);
+    signal Jr_WIRE              : std_logic;
+    signal Jr_MUXOUT            : std_logic_vector(31 downto 0);
 
 begin
 -- connect component ports and signals - port map
@@ -136,8 +142,8 @@ begin
         RegWrite    => RegWrite_WIRE,
         read_reg1   => Instruction(25 downto 21),
         read_reg2   => Instruction(20 downto 16),
-        write_reg   => RegDst_MUXOUT,
-        write_data  => MemtoReg_MUXOUT,
+        write_reg   => Jal_RegDst_MUXOUT,
+        write_data  => Jal_MemtoReg_MUXOUT,
         read_data1  => read_data1_WIRE,
         read_data2  => read_data2_WIRE
     );
@@ -148,8 +154,8 @@ begin
         RegDst      => RegDst_WIRE,
         ALUSrc      => ALUSrc_WIRE,
         Jump        => Jump_MUXSEL,
-        Jal         => open,
-        Jr          => open,
+        Jal         => Jal_WIRE,
+        Jr          => Jr_WIRE,
         Beq         => BEQ_WIRE,
         Bne         => BNE_WIRE,
         MemRead     => MemRead_WIRE,
@@ -160,7 +166,7 @@ begin
     );
         
     PC: mips_pc port map(
-        bus_in 	=> Jump_MUXOUT,
+        bus_in 	=> Jr_MUXOUT,
         bus_out => PC_OUT_WIRE,
         clock 	=> slow_clock,
         reset_n => reset
@@ -179,13 +185,14 @@ begin
         clock	=> fast_clock,
         q		=> Instruction
     );
--- mux logic
+
+    -- mux logic + combinational
     PC_PLUS_FOUR <= PC_OUT_WIRE + X"00000004";
     Branch_MUXSEL <= '1' when ((BEQ_WIRE = '1' and ALUZeroBit_WIRE = '1')
-                            or (BNE_WIRE = '1' and ALUZeroBit_WIRE = '0'))
-                else '0';
+    or (BNE_WIRE = '1' and ALUZeroBit_WIRE = '0'))
+    else '0';
     JumpAddr_WIRE <= PC_PLUS_FOUR(31 downto 28) & Instruction(25 downto 0) & "00";
-
+    
     with ALUSrc_WIRE select ALUSrc_MUXOUT <=
         read_data2_WIRE when '0',
         SignExtendImm when others;
@@ -194,9 +201,19 @@ begin
         ALU_Result_WIRE when '0',
         ReadMemData_WIRE when others;
         
+    -- FEEDS INTO WRITE DATA
+    with Jal_WIRE select Jal_MemtoReg_MUXOUT <=
+        MemtoReg_MUXOUT when '0',
+        read_data1_WIRE when others;
+        
     with RegDst_WIRE select RegDst_MUXOUT <= 
         Instruction(20 downto 16) when '0',
         Instruction(15 downto 11) when others;
+    
+    -- FEEDS INTO WRITE REGISTER
+    with Jal_WIRE select Jal_RegDst_MUXOUT <=
+        RegDst_MUXOUT when '0',
+        "11111" when others;
 
     with Branch_MUXSEL select Branch_MUXOUT <=
         PC_PLUS_FOUR when '0',
@@ -206,14 +223,19 @@ begin
         Branch_MUXOUT when '0',
         JumpAddr_WIRE when others;
 
+    -- FEEDS INTO PC REGISTER
+    with Jr_WIRE select Jr_MUXOUT <=
+        Jump_MUXOUT when '0',
+        read_data1_WIRE when others;
+
 -- connect top levels
     PC_out          <= PC_OUT_WIRE;
     Instruction_out <= Instruction;
     Read_reg1_out   <= Instruction(25 downto 21);
     Read_reg2_out   <= Instruction(20 downto 16);
-    Write_reg_out   <= RegDst_MUXOUT;
+    Write_reg_out   <= Jal_RegDst_MUXOUT;
     Read_data1_out  <= read_data1_WIRE;
     Read_data2_out  <= read_data2_WIRE;
-    Write_data_out  <= MemtoReg_MUXOUT;
+    Write_data_out  <= Jal_MemtoReg_MUXOUT;
 
 end arch;
